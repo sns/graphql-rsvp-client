2a08b0f639d880b8ee8fadd060a180ca2ea92de9
Sina <sina.it@gmail.com>
2019-03-28 00:46:57 -0400

    added some styling

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -4,25 +4,34 @@ import About from "./About";
 import Dashboard from "./Dashboard";
 import Home from "./Home";
 
+const styles = {
+    navBarContainer: {
+        height: 40,
+        backgroundColor: "black",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        color: "white",
+    } as React.CSSProperties,
+    navBarItem: {
+        color: "white",
+        display: "flex",
+        flexDirection: "row",
+        width: 300,
+        justifyContent: "center",
+        alignItems: "center",
+    } as React.CSSProperties,
+}
 export class Sidebar extends React.Component {
   render() {
     return (
       <Router>
         <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about/">About</Link>
-              </li>
-              <li>
-                <Link to="/dashboard">Dashboard</Link>
-              </li>
-            </ul>
-          </nav>
-
+          <div style={styles.navBarContainer}>
+              <Link to="/"  style={styles.navBarItem}>Home</Link>
+              <Link to="/about/" style={styles.navBarItem}>About</Link>
+              <Link to="/dashboard" style={styles.navBarItem}>Dashboard</Link>
+          </div>
           <Route path="/" exact={true} component={Home} />
           <Route path="/about/" exact={true} component={About} />
           <Route path="/dashboard" exact={true} component={Dashboard} />
